ea1796bb7b1561ba1f3adc75ad47b9f499fd3233
Misha Sharafan <sharafan911@gmail.com>
2022-12-17 21:50:36 +0200

    create task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+const ingredientCreateLi = ingEl => {
+  const ingredientItem = document.createElement('li');
+  ingredientItem.textContent = ingEl;
+  ingredientItem.classList.add('item');
+  
+  return ingredientItem;
+};
+
+const ingredientItems = ingredients.map(ingredientCreateLi);
+
+ingredientsList.append(...ingredientItems);
