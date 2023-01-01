0fd4d2ab3abcf92b3e4a65669dbd772ea21cf824
Misha Sharafan <sharafan911@gmail.com>
2023-01-01 22:59:08 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const loginForm = document.querySelector('.login-form');
+loginForm.addEventListener('submit', handleFormSubmint);
+
+function handleFormSubmint(evt) {
+    evt.preventDefault();
+
+    const {
+        elements: { email, password },
+    } = evt.currentTarget;
+
+    if (email.value === '' || password.value === '') {
+        return alert('Всі поля мають бути заповнені!');
+    }
+    console.log({ Email: email.value, Password: password.value });
+        
+    evt.currentTarget.reset();
+}
 
