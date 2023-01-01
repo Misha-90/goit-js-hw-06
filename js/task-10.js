e76cf482fd41c1aed59083b84e5eb5a0d60f8506
Misha Sharafan <sharafan911@gmail.com>
2023-01-01 23:39:38 +0200

    start 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const refs = {
+  boxesCreate: document.querySelector('#boxes'),
+  inputEl: document.querySelector('input'),
+  buttonCreatr: document.querySelector('button[data-create]'),
+  buttonDestroy: document.querySelector('button[data-destroy]'),
+}
+
